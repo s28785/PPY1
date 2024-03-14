fea742c7c223c3e0f0e827b6d4dfb61a62185c96
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2024-03-14 23:04:04 +0100

    Did task 5

--- LAB2/main.py
+++ LAB2/main.py
@@ -113,30 +113,31 @@ Extend the previous Python program to demonstrate the manipulation of lists usin
 input_numbers = input("Enter a series of space-separated integers: ")
 
 # Convert Input
-
+numbers_list = list(map(int, input_numbers.split()))
+numbers_tuple = tuple(numbers_list)
 
 # Manipulate List
-#   Append 10 to the list
-#   Insert 20 at index 2
-#   Remove one elementfrom the list
+numbers_list.append(10)  # Append 10 to the list
+numbers_list.insert(2, 20)  # Insert 20 at index 2
 
 # Attempt to Modify Tuple (this will raise an error)
 try:
-    #   Append 10 to the tuple
+    numbers_tuple.append(10)
 except AttributeError:
     print("Tuples are immutable and cannot be modified.")
 try:
-    #   Insert 20 at index 2
+    numbers_tuple.insert(2, 20)
 except AttributeError:
-    pass  # Insert operation will also raise an error
+    pass
 try:
-    #   Remove one element  from the tuple
+    numbers_tuple.remove(numbers_tuple[0])
 except AttributeError:
-    pass  # Remove operation will also raise an error
+    pass
 
 # Print Output
 print("Modified list:", numbers_list)
 print("Tuple remains unchanged:", numbers_tuple)
+
 
 """--------------------------------------------------------------------------------
 1. **Set**:
